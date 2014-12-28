aa16fa4246be440d8596a1481e993cc07b5da0f5
Azim <azimmomin6@gmail.com>
2014-12-28 15:01:23 -0600

    created initial skeleton

---
+++ Classes/Definitions.h
@@ -0,0 +1,14 @@
+/*
+ * Definitions.h
+ *
+ *  Created on: Dec 28, 2014
+ *      Author: akbar
+ */
+
+#ifndef __DEFINITIONS_H__
+#define __DEFINITIONS_H__
+
+#define DISPLAY_TIME_SPLASH_SCENE 2
+#define TRANSITION_TIME 0.5
+
+#endif /* __DEFINITIONS_H__ */
